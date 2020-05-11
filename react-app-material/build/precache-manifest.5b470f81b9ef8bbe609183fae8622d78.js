3be902bd4460faa9d7d638e8c82f2bab07c283f8
Johnathan Arroyo <64169504+jhonathanmaster@users.noreply.github.com>
2020-05-11 15:36:57 -0700

    Produccion Files

---
+++ react-app-material/build/precache-manifest.5b470f81b9ef8bbe609183fae8622d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016089bf7464807a59c45353612f72ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "575caf320ea4ed7d1f8f",
+    "url": "/static/css/main.4d588f72.chunk.css"
+  },
+  {
+    "revision": "e43ff4f9b5255307849e",
+    "url": "/static/js/2.4bd5a851.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4bd5a851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575caf320ea4ed7d1f8f",
+    "url": "/static/js/main.cb7fc49c.chunk.js"
+  },
+  {
+    "revision": "b0d04547bb0c1cd7d91f",
+    "url": "/static/js/runtime-main.90804fe5.js"
+  }
+]);
